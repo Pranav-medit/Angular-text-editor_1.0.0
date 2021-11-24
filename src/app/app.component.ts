25eded994c047260e6aa7190df6b2ff270d096d1
Pranav <pranavprati@gmail.com>
2021-11-24 12:12:03 +0530

    build app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'textEditor';
+  check1= true;
+  check2= false;
+  panelOpenState = false;
+  public Editor = ClassicEditor;
+  foods = [
+    {value: 'steak-0', viewValue: 'David Guetta'},
+    {value: 'pizza-1', viewValue: 'Peter Parker'},
+    {value: 'tacos-2', viewValue: 'Ramesh'}
+  ];
+  languages= [
+    {value: 'lang-1', viewValue: 'English'},
+    {value: 'lang-2', viewValue: 'Kannada'},
+    {value: 'lang-3', viewValue: 'Hindi'}
+  ];
+  category2 = ['Angular'];
+  category = ['Big Data'];
+  
+    // ClassicEditor.create( document.querySelector( '#editor' ), {
+    //       cloudServices: {
+    //           tokenUrl: 'https://example.com/cs-token-endpoint',
+    //           uploadUrl: 'https://your-organization-id.cke-cs.com/easyimage/upload/'
+    //       }
+    //   } )
+    //   .then(console.log("Then") )
+    //   .catch(  console.log("Then"));
+    
+  // selectedValue: string;
+  // selectedCar: string;
+
+  // foods = [
+  //   {value: 'steak-0', viewValue: 'Steak'},
+  //   {value: 'pizza-1', viewValue: 'Pizza'},
+  //   {value: 'tacos-2', viewValue: 'Tacos'}
+  // ];
+
+  // cars= [
+  //   {value: 'volvo', viewValue: 'Volvo'},
+  //   {value: 'saab', viewValue: 'Saab'},
+  //   {value: 'mercedes', viewValue: 'Mercedes'}
+  // ];
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { TagInputModule } from 'ngx-chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CKEditorModule,
+    MatExpansionModule,
+    FlexLayoutModule ,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    TagInputModule,
+    ReactiveFormsModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule { 
+  
+}
